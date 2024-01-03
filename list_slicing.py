b59b2c9619eb9c19089623193641a92ecb77cbd2
Margaret Tanzosh <margarettanzosh@nestmk12.net>
2024-01-03 18:41:10 -0500

    Create list_slicing.py

---
+++ list_slicing.py
@@ -0,0 +1,18 @@
+list1 = ["a", "b", "c", "d", "e", "f"]
+list2 = [1, 2, 3, 4, 5]
+
+print(list1[1:])
+
+# print(list2[2:4])
+
+# print(list2[-1])
+
+# print(list1[:3])
+
+# print(list1[-3: -1])
+
+# print(list1[1: 4: 2])
+
+# print(list1 + list2)
+
+# print(list1[5: 1: -1])
